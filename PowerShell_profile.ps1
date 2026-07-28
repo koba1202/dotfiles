68ae44ea9ff72cc9c3cce25e2f1e86beeaa429d8
koba1202 <show1202you@gmail.com>
2026-07-28 13:36:18 +0900

    [powershell] fzfでファイルを検索できるように

--- PowerShell_profile.ps1
+++ PowerShell_profile.ps1
@@ -1,5 +1,10 @@
 Invoke-Expression (&starship init powershell)
 
+Import-Module PSFzf
+
+# fzf の見た目の共通設定(枠の余白)。--height 等は Invoke-Fzf の引数側で指定する
+$env:FZF_DEFAULT_OPTS = '--margin=1,2'
+
 function Set-Location {
     param([string]$Path)
 
@@ -40,15 +45,61 @@ function obmemo {
     Set-Location "$HOME\memo"
 }
 
-Set-PSReadLineKeyHandler `
-    -Chord 'Alt+t' `
-    -BriefDescription 'FindFileWithFzf' `
-    -ScriptBlock {
-        $selected = git ls-files --cached --others --exclude-standard |
-            fzf
+# Set-PSReadLineKeyHandler `
+#     -Chord 'Alt+t' `
+#     -BriefDescription 'FindFileWithFzf' `
+#     -ScriptBlock {
+#         $insideGitRepo =
+#             (git rev-parse --is-inside-work-tree 2>$null) -eq 'true'
+#
+#         if (-not $insideGitRepo) {
+#             return
+#         }
+#
+#         $selected = git ls-files --cached --others --exclude-standard |
+#             & fzf.exe `
+#                 --height=50% `
+#                 --layout=reverse `
+#                 --border=rounded `
+#                 --margin=1,2 `
+#                 --prompt='Files > ' `
+#                 --preview='bat --color=always --style=numbers --line-range=:500 {}' `
+#                 --preview-window='down,55%,border-top'
+#
+#         if ($selected) {
+#             $quotedPath = "'" + $selected.Replace("'", "''") + "'"
+#             [Microsoft.PowerShell.PSConsoleReadLine]::Insert($quotedPath)
+#         }
+#     }
 
-        if ($selected) {
-            $quotedPath = "'" + $selected.Replace("'", "''") + "'"
-            [Microsoft.PowerShell.PSConsoleReadLine]::Insert($quotedPath)
-        }
+Set-PSReadLineKeyHandler -Chord 'Alt+t' -BriefDescription 'FindFileWithFzf' -ScriptBlock {
+    if ((git rev-parse --is-inside-work-tree 2>$null) -ne 'true') { return }
+
+    $out    = [System.IO.Path]::GetTempFileName()
+    $oldCmd = $env:FZF_DEFAULT_COMMAND
+    $env:FZF_DEFAULT_COMMAND = 'git ls-files --cached --others --exclude-standard'
+
+    try {
+        $fzfArgs = @(
+            '--height=50%'
+            '--layout=reverse'
+            '--border=rounded'
+            '--margin=1,2'
+            '--prompt="Files > "'
+            '--preview="bat --color=always --style=numbers --line-range=:500 {}"'
+            '--preview-window=down,55%,border-top'
+        )
+        Start-Process -FilePath 'fzf.exe' -ArgumentList $fzfArgs `
+                      -NoNewWindow -Wait -RedirectStandardOutput $out
+        $selected = (Get-Content -LiteralPath $out -Raw).Trim()
     }
+    finally {
+        $env:FZF_DEFAULT_COMMAND = $oldCmd
+        Remove-Item $out -ErrorAction Ignore
+    }
+
+    if ($selected) {
+        [Microsoft.PowerShell.PSConsoleReadLine]::Insert("'" + $selected.Replace("'","''") + "'")
+    }
+    [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
+}
